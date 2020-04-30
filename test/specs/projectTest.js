168a9f285d17aca533da0e1a6bf2bcbb1f8f216b
Dnlreygon <gerardo.reyes@wizeline.com>
2020-04-30 13:07:02 -0500

    checking failures

--- test/specs/projectTest.js
+++ test/specs/projectTest.js
@@ -1,98 +1,92 @@
 import {} from 'dotenv/config'
 import chai from 'chai'
 import chaitHttp from 'chai-http'
-describe('Projects API ENDPOINTS Tests',() => {
-    const VIRTUAL_TOKEN = process.env.VIRTUAL_TOKEN
-    const PROJECTS_PATH=process.env.PROJECTS_PATH 
-  beforeEach(()=>{
-      chai.use(chaitHttp)
-      chai.should()
+describe('Projects API ENDPOINTS Tests', () => {
+  const VIRTUAL_TOKEN = process.env.VIRTUAL_TOKEN
+  const PROJECTS_PATH = process.env.PROJECTS_PATH
+  beforeEach(() => {
+    chai.use(chaitHttp)
+    chai.should()
   })
 
-it('Get ALL Projects', (done) => {
-        chai.request(process.env.URL_BASE)
-        .get(process.env.PROJECTS_PATH )
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .end((err,response) => {
-            response.should.have.status(200)
-            done()
-        })
-    })
-
-it('Create a New Project' , () => {
-        chai.request(process.env.URL_BASE)
-        .post(PROJECTS_PATH)
-        .send({
-            
-            "name" : 'New Project 1',
-            "order" : 2
-        })
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .set('Content-Type','application/json')
-        .end((err,response) => {
-            response.should.have.status(200)
-            console.log(response.body.name)
-            response.body.should.have.property('id')
-            response.body.should.have.property('order')
-            response.body.should.have.property('color')
-            response.body.name.should.be.equal('New Project 1')
-            response.body.should.have.property('comment_count')
-            response.body.should.have.property('shared')
-            
-        })
-    })
-    
-
-it('Get The New Project', () => {
+  it('Get ALL Projects', (done) => {
     chai.request(process.env.URL_BASE)
-    .get(PROJECTS_PATH)
-    .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-    .end((err,response) => {
+      .get(process.env.PROJECTS_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .end((response) => {
         response.should.have.status(200)
-        })
-    })
-    
+        done()
+      })
+  })
 
-it('Update The New Project' , (done) => {
-    //codigo get 
+  it('Create a New Project', () => {
     chai.request(process.env.URL_BASE)
-        .get(process.env.PROJECTS_PATH)
-        .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-        .set('Content-Type', 'application/json')
-        .end((err, response) => {
-            let projectId=response.body[2].id
-            chai.request(process.env.URL_BASE)
-                .post(PROJECTS_PATH + "/"+projectId)
-                .send({
-                    "name": 'New Project 3.7',
-                    "order": 2
-                })
-                .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-                .set('Content-Type', 'application/json')
-                .end((err, responsePost) => {
-                    responsePost.should.have.status(204)
-                    done()
-                })
-        })
-    })
+      .post(PROJECTS_PATH)
+      .send({
+        name: 'New Project 1',
+        order: 2
+      })
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        response.should.have.status(200)
+        console.log(response.body.name)
+        response.body.should.have.property('id')
+        response.body.should.have.property('order')
+        response.body.should.have.property('color')
+        response.body.name.should.be.equal('New Project 1')
+        response.body.should.have.property('comment_count')
+        response.body.should.have.property('shared')
+      })
+  })
 
-    
+  it('Get The New Project', () => {
+    chai.request(process.env.URL_BASE)
+      .get(PROJECTS_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .end((response) => {
+        response.should.have.status(200)
+      })
+  })
 
-    it('Delete The New Project', () => {
-        //codigo get 
+  it('Update The New Project', (done) => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.PROJECTS_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        const projectId = response.body[2].id
         chai.request(process.env.URL_BASE)
-            .get(process.env.PROJECTS_PATH)
-            .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-            .set('Content-Type', 'application/json')
-            .end((err, response) => {
-        let projectId = response.body[2].id
-        console.log("delete the project " + response.body[2].name)
+          .post(PROJECTS_PATH + '/' + projectId)
+          .send({
+            name: 'New Project 3.7',
+            order: 2
+          })
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .set('Content-Type', 'application/json')
+          .end((responsePost) => {
+            responsePost.should.have.status(204)
+            done()
+          })
+      })
+  })
+
+  it('Delete The New Project', () => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.PROJECTS_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        const projectId = response.body[2].id
+        console.log('delete the project ' + response.body[2].name)
         chai.request(process.env.URL_BASE)
-        .delete(PROJECTS_PATH + '/' + projectId)
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .end((err,response) => {
+          .delete(PROJECTS_PATH + '/' + projectId)
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .end((response) => {
             response.should.have.status(204)
-        })
-    })
-  })  
+          })
+      })
+  })
 })
--- test/specs/taskTest.js
+++ test/specs/taskTest.js
@@ -1,142 +1,139 @@
 import {} from 'dotenv/config'
 import chai from 'chai'
 import chaitHttp from 'chai-http'
-describe('Projects API ENDPOINTS Tests',() => {
-    const VIRTUAL_TOKEN = process.env.VIRTUAL_TOKEN
-    const PROJECTS_PATH=process.env.PROJECTS_PATH 
-    const TASK_PATH=process.env.TASK_PATH
+describe('Projects API ENDPOINTS Tests', () => {
+  const VIRTUAL_TOKEN = process.env.VIRTUAL_TOKEN
+  const TASK_PATH = process.env.TASK_PATH
 
-  beforeEach(()=>{
-      chai.use(chaitHttp)
-      chai.should()
+  beforeEach(() => {
+    chai.use(chaitHttp)
+    chai.should()
   })
 
-    it('Get All Tasks', (done) => {
+  it('Get All Tasks', (done) => {
+    chai.request(process.env.URL_BASE)
+      .get(process.env.TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .end((response) => {
+        response.should.have.status(200)
+        done()
+      })
+  })
+
+  it('Create a New Task', () => {
+    chai.request(process.env.URL_BASE)
+      .post(TASK_PATH)
+      .send({
+        content: 'New Task 1',
+        order: 33
+      })
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        response.should.have.status(200)
+        console.log(response.body.content)
+        response.body.should.have.property('id')
+        response.body.should.have.property('order')
+        response.body.content.should.be.equal('New Task 1')
+        response.body.should.have.property('comment_count')
+      })
+  })
+
+  it('Get The New Task', (done) => {
+    chai.request(process.env.URL_BASE)
+      .get(TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .end((response) => {
+        response.should.have.status(200)
+        done()
+      })
+  })
+
+  it('Update The New Task', (done) => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        const projectId = response.body[response.body.length - 1].id
         chai.request(process.env.URL_BASE)
-        .get(process.env.TASK_PATH)
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .end((err,response) => {
-            response.should.have.status(200)
+          .post(TASK_PATH + '/' + projectId)
+          .send({
+            content: 'New Task 1.5',
+            order: 33
+          })
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .set('Content-Type', 'application/json')
+          .end((responsePost) => {
+            responsePost.should.have.status(204)
             done()
-        })
-    })
+          })
+      })
+  })
 
-    it('Create a New Task' , () => {
+  it('Close The New Task', (done) => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        const projectId = response.body[response.body.length - 1].id
+        console.log('close the task ' + response.body[response.body.length - 1].id)
         chai.request(process.env.URL_BASE)
-        .post(TASK_PATH)
-        .send({
-            
-            "content" : 'New Task 1',
-            "order" : 33
-        })
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .set('Content-Type','application/json')
-        .end((err,response) => {
-            response.should.have.status(200)
-            console.log(response.body.content)
-            response.body.should.have.property('id')
-            response.body.should.have.property('order')
-            response.body.content.should.be.equal('New Task 1')
-            response.body.should.have.property('comment_count')
-            
-        })
-    })
-
-    it('Get The New Task', (done) => {
-        chai.request(process.env.URL_BASE)
-        .get(TASK_PATH)
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .end((err,response) => {
-            response.should.have.status(200)
+          .post(TASK_PATH + '/' + projectId + '/close')
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .end((responsePost) => {
+            responsePost.should.have.status(204)
             done()
-            })
-        })
+          })
+      })
+  })
 
-    it('Update The New Task' , (done) => {
-            //codigo get 
+  it('Reopen The New Task', (done) => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        // codigo close
+        const projectId = response.body[response.body.length - 1].id
         chai.request(process.env.URL_BASE)
-        .get(process.env.TASK_PATH)
-        .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-        .set('Content-Type', 'application/json')
-        .end((err, response) => {
-            let projectId=response.body[response.body.length-1].id
+          .post(TASK_PATH + '/' + projectId + '/close')
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .end((responsePost) => {
+            responsePost.should.have.status(204)
+            // codigo re-open
+            console.log('Reopen the task ' + projectId)
             chai.request(process.env.URL_BASE)
-                .post(TASK_PATH + "/"+projectId)
-                .send({
-                    "content": 'New Task 1.5',
-                     "order": 33
-                    })
-                .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-                .set('Content-Type', 'application/json')
-                .end((err, responsePost) => {
-                    responsePost.should.have.status(204)
-                    done()
-                    })
-                })
-            })
+              .post(TASK_PATH + '/' + projectId + '/reopen')
+              .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+              .end((responsePost) => {
+                responsePost.should.have.status(204)
+                done()
+              })
+          })
+      })
+  })
 
-    it('Close The New Task' , (done) => {
-        //codigo get 
+  it('Delete The New Task', (done) => {
+    // codigo get
+    chai.request(process.env.URL_BASE)
+      .get(process.env.TASK_PATH)
+      .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+      .set('Content-Type', 'application/json')
+      .end((response) => {
+        const projectId = response.body[response.body.length - 1].id
+        console.log('delete the Task: ' + response.body[response.body.length - 1].content)
         chai.request(process.env.URL_BASE)
-        .get(process.env.TASK_PATH)
-        .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-        .set('Content-Type', 'application/json')
-        .end((err, response) => {
-            let projectId = response.body[response.body.length - 1].id
-            console.log("close the task " + response.body[response.body.length-1].id)
-                chai.request(process.env.URL_BASE)
-                .post(TASK_PATH + "/"+projectId + "/close")
-                .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-                .end((err, responsePost) => {
-                    responsePost.should.have.status(204)
-                    done()
-                    })
-                })
-        })
-
-    it('Reopen The New Task' , (done) => {
-        //codigo get 
-        chai.request(process.env.URL_BASE)
-        .get(process.env.TASK_PATH)
-        .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-        .set('Content-Type', 'application/json')
-        .end((err, response) => {
-            //codigo close
-            let projectId = response.body[response.body.length - 1].id
-            chai.request(process.env.URL_BASE)
-                .post(TASK_PATH + "/" + projectId + "/close")
-                .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-                .end((err, responsePost) => {
-                    responsePost.should.have.status(204)
-                    //codigo re-open
-                    console.log("Reopen the task " + projectId)
-                    chai.request(process.env.URL_BASE)
-                        .post(TASK_PATH + "/" + projectId + "/reopen")
-                        .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-                        .end((err, responsePost) => {
-                            responsePost.should.have.status(204)
-                            done()
-                        })
-                   })
-            })
-        })
-
-    it('Delete The New Task', (done) => {
-        //codigo get 
-         chai.request(process.env.URL_BASE)
-            .get(process.env.TASK_PATH)
-            .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
-            .set('Content-Type', 'application/json')
-            .end((err, response) => {
-                let projectId = response.body[response.body.length - 1].id
-        console.log("delete the Task: " + response.body[response.body.length-1].content)
-        chai.request(process.env.URL_BASE)
-        .delete(TASK_PATH + '/' + projectId)
-        .set('Authorization','Bearer '+VIRTUAL_TOKEN)
-        .end((err,response) => {
+          .delete(TASK_PATH + '/' + projectId)
+          .set('Authorization', 'Bearer ' + VIRTUAL_TOKEN)
+          .end((response) => {
             response.should.have.status(204)
             done()
-            })
-        })
-    })  
+          })
+      })
+  })
 })
